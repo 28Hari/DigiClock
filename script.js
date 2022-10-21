e2f687ab0a84ae90732552e0ff227b4a3396b20b
Hariharan K <93050905+28Hari@users.noreply.github.com>
2022-10-21 10:54:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const clock=document.querySelector('.clock')
+function runClock(){
+    let time=new Date();
+    
+    let hrs=time.getHours()
+    
+    let min=time.getMinutes()
+    
+    let sec=time.getSeconds()
+ 
+    let txt="am"
+    if(hrs>12){
+        hrs=hrs-12
+        txt="pm"
+    }else if(hrs==0){
+        hrs=12
+        txt="am"
+    }
+    hrs=hrs<10?'0'+hrs:hrs
+    min=min<10?'0'+min:min
+    sec=sec<10?'0'+sec:sec
+    clock.innerHTML=(`${hrs} : ${min} : ${sec} ${txt}`)
+}
+runClock()
+setInterval(runClock,1000)
